0d1878361c0f31e7bf44c4d31fb3a7d73f05d91c
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2024-02-03 01:37:50 +0530

    setup cross-env, adding middlewares

--- src/app.js
+++ src/app.js
@@ -1,11 +1,52 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize"
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
 
+//dot env confog
+dotenv.config();
 
 //create react app
 
 const app = express();
 
-app.get('/', (req, res) => {
+//Morgan
+if(process.env.NODE_ENV !== "production"){
+    app.use(morgan("dev"));
+}
+
+//helmet
+app.use(helmet());
+
+//parse json request url
+app.use(express.json());
+
+//parse json request body
+app.use(express.urlencoded({extended: true}));
+
+//sanitize request data
+app.use(mongoSanitize());
+
+//Enable cooke parser
+app.use(cookieParser());
+
+//gzip compression
+app.use(compression());
+
+//file upload
+app.use(fileUpload({
+    useTempFiles : true,  //Use temp files for
+}))
+
+//cors
+app.use(cors())
+
+app.post('/test', (req, res) => {
     res.send("hello from server");
 });
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import app from "./app.js"
-import dotenv from "dotenv";
 
-//dot env confog
-dotenv.config();
 
 //environment varable
 const PORT = process.env.PORT || 8000;
+
 
 app.listen(PORT, () => {
     console.log(`server running at port ${PORT}...`);
