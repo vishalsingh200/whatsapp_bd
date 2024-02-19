598e0b1f50d36a089a43db921ce1fbd094dd6326
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2024-02-19 23:54:49 +0530

    generate access and refreshs, store refresh token in cookies in the server

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import { createUser } from "../services/auth.service.js";
+import { generateToken } from "../services/token.service.js";
+
 
 export const register = async (req, res, next) => {
     try{
@@ -10,7 +12,37 @@ export const register = async (req, res, next) => {
             status,
             password,
         });
-        res.json(newUser);
+        const access_token = await generateToken(
+            {userId: newUser._id}, 
+            "1d", 
+            process.env.ACCESS_TOKEN_SECRET
+        );
+        const refresh_token = await generateToken(
+            {userId: newUser._id}, 
+            "30d", 
+            process.env.REFRESH_TOKEN_SECRET
+        );
+
+        res.cookie('refreshtoken', refresh_token, {
+            httpOnly: true,
+            path: "/api/v1/auth/refreshtoken",
+            maxAge: 30*24*60*60*100, //30days
+        }
+        );
+
+        console.table({ access_token, refresh_token})
+
+        res.json({
+            message: "register success.",
+            access_token,
+            user: {
+                _id: newUser._id,
+                name: newUser.name,
+                email: newUser.email,
+                picture: newUser.picture,
+                status: newUser.status,
+            },
+        });
     }
     catch(error){
         next(error);
