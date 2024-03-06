fb78cedeff31d080731d1d3390e94920ef5e0ce4
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2024-03-06 11:29:17 +0530

    auth middleware

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import trimRequest from "trim-request"
 import { login, logout, refreshToken, register } from "../controllers/auth.controller.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
@@ -8,6 +9,9 @@ router.route("/register").post(trimRequest.all, register);
 router.route("/login").post(trimRequest.all, login);
 router.route("/logout").post(trimRequest.all, logout);
 router.route("/refreshtoken").post(trimRequest.all, refreshToken);
+router.route("/testingauthMiddlware").get(trimRequest.all, authMiddleware,(req, res) => {
+    res.send(req.user);
+});
 
 
 export default router;
