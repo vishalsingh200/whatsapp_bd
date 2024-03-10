e3fbe743fa48449dd55967e73d4e1e27894f10dd
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2024-03-11 00:57:29 +0530

    updated

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -36,13 +36,13 @@ export const register = async (req, res, next) => {
 
         res.json({
             message: "register success.",
-            access_token,
             user: {
                 _id: newUser._id,
                 name: newUser.name,
                 email: newUser.email,
                 picture: newUser.picture,
                 status: newUser.status,
+                access_token,
             },
         });
     }
@@ -75,13 +75,13 @@ export const login = async (req, res, next) => {
         
         res.json({
             message: "login success.",
-            access_token,
             user: {
                 _id: user._id,
                 name: user.name,
                 email: user.email,
                 picture: user.picture,
                 status: user.status,
+                access_token,
             },
         });
     }
@@ -117,13 +117,13 @@ export const refreshToken = async (req, res, next) => {
             process.env.ACCESS_TOKEN_SECRET
         );
         res.json({
-            access_token,
             user: {
                 _id: user._id,
                 name: user.name,
                 email: user.email,
                 picture: user.picture,
                 status: user.status,
+                access_token,
             },
         });
     }
